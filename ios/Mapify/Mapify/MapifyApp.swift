7954c6fd72e654702cecb1149f7a02f874c78fb4
Steve Galbraith <galbernator@gmail.com>
2024-09-26 09:12:23 -0600

    Initial commit with folder structure

---
+++ ios/Mapify/Mapify/MapifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapifyApp.swift
+//  Mapify
+//
+//  Created by Steve Galbraith on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MapifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Mapify")
+        }
+    }
+}
---
+++ ios/Mapify/MapifyTests/MapifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapifyTests.swift
+//  MapifyTests
+//
+//  Created by Steve Galbraith on 9/26/24.
+//
+
+import Testing
+@testable import Mapify
+
+struct MapifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
